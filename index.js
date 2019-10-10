a101387f734b1a57315dccd49bbd2b2f40c1eb10
jiangtj <116749895@qq.com>
2019-10-10 13:03:18 +0800

    Fix

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const utils = require('hexo-cake-utils')(hexo, __dirname);
 
 hexo.extend.filter.register('theme_inject', function(injects) {
-  utils.defaultConfigFile('google_analytics', 'default.yaml');
-  injects.head.file('google-analytics', utils.getFilePath('google-analytics.swig'), {}, {cache: true});
+  let config = utils.defaultConfigFile('google_analytics', 'default.yaml');
+  injects.head.file('google-analytics', utils.getFilePath('google-analytics.swig'), config, {cache: true, only: true});
 });
